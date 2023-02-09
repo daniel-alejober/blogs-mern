c8f4b14976328c565e1c3769dfff4b438d05b8e2
Daniel Alejo <daniel-alejo94@hotmail.com>
2023-02-09 15:07:08 -0600

    save project

--- client/src/context/ArticlesProvider.jsx
+++ client/src/context/ArticlesProvider.jsx
@@ -4,17 +4,22 @@ import clienteAxios from "../Utils/clientAxios";
 const ArticlesContext = createContext();
 
 const ArticleProvider = ({ children }) => {
+  const [dataArticles, setDataArticles] = useState([]);
+
   const getAllArticles = async () => {
     try {
-      const data = await clienteAxios.get("/api/v1/artitle/all-articles");
-      console.log(data);
+      const { data } = await clienteAxios.get("/api/v1/artitle/all-articles");
+
+      if (data.success) {
+        setDataArticles(data.articles);
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <ArticlesContext.Provider value={{ getAllArticles }}>
+    <ArticlesContext.Provider value={{ getAllArticles, dataArticles }}>
       {children}
     </ArticlesContext.Provider>
   );
